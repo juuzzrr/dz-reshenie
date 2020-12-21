375bd3d860b918bfaa6a93e086265a2acd45dfd0
juuzzrr <programmersergey05@gmail.com>
2020-12-21 20:50:33 +0300

    dz

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <UserCard 
+    v-bind:cats="cats"
+    />
+  </div>
+</template>
+
+<script>
+import UserCard from "./components/UserCard.vue";
+export default {
+  name: "app",
+  components: {
+    UserCard
+  },
+  data() {
+    return{
+      info: [],
+    }
+  },
+  methods: {
+    update() {
+    this.axios.get(" http://188.225.47.187/api/cats/random-fact.php")
+      .then(response => {
+        this.info = response.data;
+      });
+    }
+  },
+  mounted() {
+    this.update()
+  }
+};
+</script>
+<style>
+  button{
+    width:200px;
+    height:200px;
+  }
+</style>
